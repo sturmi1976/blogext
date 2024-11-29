c812f503fb803b58f651da09112da8894304b6d5
Andre Lanius <a-lanius@web.de>
2024-11-29 18:47:19 +0100

    .

--- ext_emconf.php
+++ ext_emconf.php
@@ -3,15 +3,16 @@
 $EM_CONF[$_EXTKEY] = [
     'title' => 'Simple TYPO3 Blog',
     'description' => 'Simple TYPO3 blog extension.',
-    'version' => '1.0.2',
+    'category' => 'Frontend Plugins', 
+    'version' => '1.0.3',
     'state' => 'stable',
     'author' => 'Andre Lanius',
     'author_email' => 'info@andre-lanius.site',
-    'author_company' => 'Andre Lanius - TYPO3 Freelancer',
+    'author_company' => 'Andre Lanius Web',
     'constraints' => [
         'depends' => [
             'typo3' => '13.0.0-13.4.99',
-            'php' => '8.2.0-8.3.99',
+            'php' => '8.2.0-8.3.99', 
         ],
     ],
     'autoload' => [
